e1d5c21b80990f785a1a2761a2261c321852aee7
0-TB <olliebrindley15@gmail.com>
2025-05-23 13:15:59 +0100

    Create InteractiveSheets.tsx

---
+++ InteractiveSheets.tsx
@@ -0,0 +1,8 @@
+// InteractiveSheets.tsx
+import React, { useState, useRef, useEffect } from 'react';
+import { X, Home, Settings, Info, Menu, ChevronLeft, ChevronRight } from 'lucide-react';
+
+// Copy the entire component code from the artifact here
+export default function InteractiveSheets() {
+  // ... (all the component code from our artifact)
+}
